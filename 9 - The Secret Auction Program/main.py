99f5df73388ac737d0afeaddab3e6a3d932a2996
Sthefany Spina <125087195+sthefanyspina@users.noreply.github.com>
2024-11-16 08:09:31 -0300

    Create main.py

---
+++ 9 - The Secret Auction Program/main.py
@@ -0,0 +1,33 @@
+from replit import clear
+from art import logo
+
+auction_info = []
+auction = True
+
+def add_new_bidder(bidder_name, bidder_amount):
+    new_bidder = {}
+    new_bidder["bid"] = bidder_amount
+    new_bidder["name"] = bidder_name
+    auction_info.append(new_bidder)
+
+while auction:
+    print(logo)
+    print("Welcome tot he private bidding auction")
+    name = input("What is your name?: ").title()
+    bid = float(input("How much would you like to bid?: ¨$"))
+    add_new_bidder(bidder_name = name, bidder_amount = bid)
+
+others = input("Are they any other bidders for this auctuon? Type 'yes or 'no'. \n") 
+if others == 'no':
+    auction = False
+    highest_bidder = 0
+    count = -1
+    for e in auction_info:
+        if e['bid'] > highest_bidder:
+            highest_bidder = e['bid']
+            count += 1
+    winner = auction_info[count]['name']
+    print(f"The highest bidder is {winner} with a ${highest_bidder}")
+    print("Thank you for your participation.")
+else:
+    clear()
